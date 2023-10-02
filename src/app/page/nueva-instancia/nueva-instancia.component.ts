728e6d165f79139d16ce87d0f691b5914b0346f7
Cintia <cintialeal31@gmail.com>
2023-10-02 20:14:38 -0300

    Cambios

--- src/app/page/nueva-instancia/nueva-instancia.component.ts
+++ src/app/page/nueva-instancia/nueva-instancia.component.ts
@@ -4,19 +4,23 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Instancia } from '../../modelos/instancia';
 import { InstanciaNueva } from '../../modelos/instanciaNueva';
 import { LoginService } from '../../servicios/login.service';
-
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Observable, subscribeOn, Subscriber } from 'rxjs';
 @Component({
   selector: 'app-nueva-instancia',
   templateUrl: './nueva-instancia.component.html',
   styleUrls: ['./nueva-instancia.component.scss']
 })
 export class NuevaInstanciaComponent {
+  public base64Image: any;
   constructor(private api: LoginService, private alerta: MatSnackBar ){ }
 //FORM desde el HTML
   registrarForm = new FormGroup({
     nombre: new FormControl('',Validators.required),
     esquemaColores: new FormControl('',Validators.required),
-    tematica: new FormControl('',Validators.required)
+    tematica: new FormControl('',Validators.required),
+    logo: new FormControl('',Validators.required),
   });
 
   onRegistrar() {
@@ -25,7 +29,7 @@ export class NuevaInstanciaComponent {
       nombre: this.registrarForm.controls["nombre"].value  ? this.registrarForm.controls["nombre"].value : " ",
       esquemaColores: 1,
       tematica: this.registrarForm.controls["tematica"].value  ? this.registrarForm.controls["tematica"].value : " ",
-      logo:  "logo ",
+      logo:  this.base64Image,
       url:  "logo ",
       activo:true,
       privacidad:1
@@ -38,6 +42,44 @@ export class NuevaInstanciaComponent {
     this.alerta.open("Creada con éxito", "OK!");
   }
 
+  //INI PARA IMG COM BASE 64
+  convertToBase64(file: File) {
+    console.log(file);
+    const observable = new Observable((subscriber: Subscriber<any>) => {
+      this.readFile(file, subscriber);
+    })
+
+    observable.subscribe((d) => {
+      this.base64Image = d;
+      console.log(this.base64Image);
+    })
+  }
+
+  readFile(file: File, subscriber: Subscriber<any>) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file)
+
+    fileReader.onload = () => {
+      subscriber.next(fileReader.result)
+      subscriber.complete()
+    }
+    fileReader.onerror = () => {
+      subscriber.error()
+    }
+  }
   
+  onFileSelected(event: any): void {
+     this.convertToBase64(event.target.files[0]);
+   }
+  //FIN PARA IMG COM BASE 64
+  showImage() {
+    if (this.base64Image) {
+      // Devuelve la imagen base64 como una URL de datos
+      return `data:image/png;base64,${this.base64Image}`;
+    } else {
+      // Puedes establecer una URL de imagen predeterminada o un mensaje de error aquí
+      return 'ruta_a_imagen_predeterminada_o_mensaje_de_error';
+    }
+  }
 }
 
