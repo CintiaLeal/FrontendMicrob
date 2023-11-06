2bc0db9855bcad9bb59267a5149f6188c77b651f
Cintia <cintialeal31@gmail.com>
2023-11-06 19:49:56 -0300

    Cambios header

--- src/app/page/headerpage/header-platform-administrator/header-platform-administrator.component.ts
+++ src/app/page/headerpage/header-platform-administrator/header-platform-administrator.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
 import { AppService } from 'src/app/servicios/app.service';
+import { InstanciaRetorno } from 'src/app/modelos/instanciaRetorno';
+
 
 @Component({
   selector: 'app-header-platform-administrator',
@@ -10,6 +12,20 @@ import { AppService } from 'src/app/servicios/app.service';
 })
 export class HeaderPlatformAdministratorComponent {
   constructor(private router: Router,private api: AppService, private appPrincipal:AppComponent) {}
+  instanciaActualParaHeader: InstanciaRetorno | null=null;
+  idinstancia: any | null = null;
+  modoOscuro = false;
+  userName: string | null = null;
+
+  ngOnInit(): void {
+    this.userName = localStorage.getItem('userName');
+    this.idinstancia = localStorage.getItem('idInstancia');
+    this.api.getInstanciaPorId(this.idinstancia).subscribe({
+      next: value => this.instanciaActualParaHeader = value,
+      error: err => {
+      }
+    });
+}
 
 
   logout() {
